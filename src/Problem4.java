5c7254c1d216d80ebad6c0864225f7476cd49395
chisroxana31 <chisroxana31@yahoo.com>
2023-10-19 22:55:54 +0300

    Sixth commit(third for today).

--- src/Problem4.java
+++ src/Problem4.java
@@ -9,7 +9,7 @@ public class Problem4 {
         return billigste;
     }
 
-    public static int teuersteGegenstand(int[] tastaturen, int[] usbLaufwerke]) {
+    public static int teuersteGegenstand(int[] tastaturen, int[] usbLaufwerke) {
         int teureste = Integer.MIN_VALUE;
         for (int preis : tastaturen) {
             if (preis > teureste) {
@@ -24,5 +24,25 @@ public class Problem4 {
             }
         }
         return teureste;
+    }
+
+    public static int teuersteUSBLaufwerk(int[] usbLaufwerke, int budget) {
+        int teuerste = -1;
+        for (int preis : usbLaufwerke) {
+            if (preis <= budget && preis > teuerste) {
+                teuerste = preis;
+            }
+        }
+        return teuerste;
+    }
+    public static int geldbetragMarkus(int budget, int[] tastaturen, int[] usbLaufwerke) {
+        int billigsteTastatur = billigsteTastatur(tastaturen);
+        int teuersteUSBLaufwerk = teuersteUSBLaufwerk(usbLaufwerke, budget);
+
+        if (billigsteTastatur == Integer.MAX_VALUE || teuersteUSBLaufwerk == -1) {
+            return -1;
+        }
+
+        return billigsteTastatur + teuersteUSBLaufwerk;
     }
 }
